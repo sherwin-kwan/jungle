b38e29fba920028dde2770ca01b9c70e9d4b67f7
Sherwin Kwan <sherwin.kwan92@gmail.com>
2020-11-11 19:09:24 +0000

    add admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,16 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    @totals = {
+      products: Product.count,
+      categories: Category.count,
+      orders: Order.count,
+      products_purchased: LineItem.distinct(:product_id).count,
+    }
+    products_table_query = "SELECT products.id, products.name, COUNT(line_items.id)
+      FROM line_items 
+      JOIN products ON line_items.product_id = products.id
+      GROUP BY products.id"
+    @products_table_array = ActiveRecord::Base.connection.execute(products_table_query)
   end
 end
