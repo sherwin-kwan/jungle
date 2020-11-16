0626091aa5a693397ca8b5b318b1e047d986320a
Sherwin Kwan <sherwin.kwan92@gmail.com>
2020-11-16 23:13:12 +0000

    add rspec tests for product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
 
+  # Note: The money-rails gem automatically interprets a column name called "price_cents" to mean "store a price in cents"
+  # So it will automatically multiply a price entered in the params by 100 and save it here
   monetize :price_cents, numericality: true
   mount_uploader :image, ProductImageUploader
 
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  it 'should successfully save a new user if all required fields are present' do
+    
+  end
+
+end
