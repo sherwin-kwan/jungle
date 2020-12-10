a62cbf0c150b1ae39565c22fec94899a4b857d70
Sherwin <sherwin.kwan92@gmail.com>
2020-12-10 14:09:00 -0800

    move images and stylesheets from asset pipeline to webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,8 +11,12 @@
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
+// Include stylesheets in Webpack bundle
+import 'stylesheets/application';
 
 console.log('Hello World from Webpacker')
+
