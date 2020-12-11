42817fc13f7490cea9aba1f1e62f7a0e0739e666
Sherwin <sherwin.kwan92@gmail.com>
2020-12-10 17:07:23 -0800

    bootstrap issues

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,13 @@
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 
+// Libraries to import
+
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+
+
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
 
