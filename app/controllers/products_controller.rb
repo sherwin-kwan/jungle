0f4b7b244c1874b457fe6e84d579de05c1c54e50
Sherwin <sherwin.kwan92@gmail.com>
2020-12-15 23:52:25 -0800

    add categories sidebar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
+
 # Controller class to display products (dishes)
 class ProductsController < ApplicationController
   def index
-    @products = Product.all.order(created_at: :desc)
+    @products = ApplicationHelper.shuffle(Product.all.to_a)
+    @categories = Category.select(:id, :name)
   end
 
   def show
