89d70a4f2e52d37d16995ad760b5818cb651b2f0
Sherwin <sherwin.kwan92@gmail.com>
2020-12-16 12:41:52 -0800

    update product images to use image_pack_tag and add alts; migrate database to add item FK in orders table

---
+++ db/migrate/20201216203538_connect_users_to_orders_table.rb
@@ -0,0 +1,5 @@
+class ConnectUsersToOrdersTable < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :orders, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_062655) do
+ActiveRecord::Schema.define(version: 2020_12_16_203538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2020_12_09_062655) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "stripe_charge_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -76,6 +78,7 @@ ActiveRecord::Schema.define(version: 2020_12_09_062655) do
 
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "categories"
   add_foreign_key "sales", "products"
 end
