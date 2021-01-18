a79582c2e541992c62772b31690ab7aa85c8a347
Sherwin <sherwin.kwan92@gmail.com>
2021-01-18 13:10:28 -0800

    fix bug causing cart to empty when user navigates to homepage

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -26,14 +26,12 @@ class CartsController < ApplicationController
   def add_item
     product_id = params[:product_id].to_s
     modify_cart_delta(product_id, +1)
-
     redirect_back(fallback_location: root_path)
   end
 
   def remove_item
     product_id = params[:product_id].to_s
     modify_cart_delta(product_id, -1)
-
     redirect_back(fallback_location: root_path)
   end
 
